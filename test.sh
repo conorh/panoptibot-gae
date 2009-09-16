250539094685242e6f47a2f3b42177d87010c89b
Conor Hunt <conor.hunt+git@gmail.com>
2009-09-16 00:03:46 -0400

    added test directory

--- test.sh
+++
@@ -1,1 +0,0 @@
-curl -d "from=from%40test.com%2Fstuff&to=receiver%40test.com%2Fblah&body=%2Fapp" http://localhost:8080/_ah/xmpp/message/chat/
---
+++ test/test.sh
@@ -0,0 +1,2 @@
+curl -d "from=from%40test.com%2Fhist&to=receiver%40test.com%2Fblah&body=%2Fapp" http://localhost:8080/_ah/xmpp/message/chat/
+curl -d "from=from%40test.com%2Fhist&to=receiver%40test.com%2Fblah&body=%2Fwho" http://localhost:8080/_ah/xmpp/message/chat/
