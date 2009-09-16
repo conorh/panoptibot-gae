e567bea19613144539b8dcbf0858cb17d8a9e108
Conor Hunt <conor.hunt+git@gmail.com>
2009-09-16 00:36:26 -0400

    try user timezone if available

--- main.py
+++ main.py
@@ -12,13 +12,17 @@ from pytz import tz_helper
 class MainPage(webapp.RequestHandler):
   def get(self):
     user = users.get_current_user()
-    
-    if not bot.ChatUser.gql("WHERE jid = :1", user.email()).get():
+    chat_user = bot.ChatUser.gql("WHERE jid = :1", user.email()).get()
+    if not chat_user:
       self.response.out.write("access denied")
     else:
       chat_log = bot.MessageLog.all().order('-created_at').fetch(500)
       
-      new_zone = tz_helper.timezone('US/Eastern')
+      if chat_user.timezone != None:
+        new_zone = tz_helper.timezone(chat_user.timezone)
+      else:
+        new_zone = tz_helper.timezone('US/Eastern')
+      
       utc = tz_helper.timezone('UTC')
       for message in chat_log:
         message.created_at = message.created_at.replace(tzinfo=utc).astimezone(new_zone)
